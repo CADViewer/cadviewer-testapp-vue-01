5da095503c3aa20c72e7789a08f4a00840037044
CADViewer <61973235+CADViewer@users.noreply.github.com>
2022-11-19 09:21:13 +0100

    Update CADViewerCanvas.vue

--- src/components/CADViewerCanvas.vue
+++ src/components/CADViewerCanvas.vue
@@ -21,8 +21,8 @@ var  handle_selector = false;
 var  current_selected_handle = "";
 
 
-// We should to define all the CADViewer methods in which we are getting information return from CADViewer
-// THEY CAN BE PLACEHOLDERS ONLY 
+//  We should to define all the CADViewer methods in which we are getting information return from CADViewer
+//  THEY CAN BE PLACEHOLDERS ONLY 
 
 
 //export function cvjs_OnLoadEnd(){
